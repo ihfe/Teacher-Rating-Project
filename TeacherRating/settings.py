a65283f124c6543f27ed312f2762390858bddf39
Yuanzhe <2967969y@student.gla.ac.uk>
2025-03-20 14:25:54 +0000

    第八次

--- TeacherRating/settings.py
+++ TeacherRating/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'i$di0kle*__+01#18ht#qs$y57em8@=-n-wx12_e6m0b#n%djb'
+SECRET_KEY = 'i$di0kle*__+01#18ht#qs$y57em8@=-n-wx12_e6m0b#n%djb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
